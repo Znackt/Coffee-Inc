7c92690e3354077a58acff4ea974840869d12fe4
Znackt <Znackt@proton.me>
2024-09-03 23:31:12 +0530

    errors resolved!

--- contracts/Coffee_Inc.sol
+++ contracts/Coffee_Inc.sol
@@ -5,10 +5,10 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Capped.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 
-contract CoffeeCode is ERC20Capped, ERC20Burnable {
+contract Coffee_Inc is ERC20Capped, ERC20Burnable {
     address payable public owner;
     uint256 public blockReward;
-    constructor(uint256 cap, uint256 reward) ERC20("Coffee Code", "CCODE") ERC20Capped (cap * 10 ** decimals()) {
+    constructor(uint256 cap, uint256 reward) ERC20("Coffee_Inc", "COFI") ERC20Capped (cap * 10 ** decimals()) {
         owner = payable(msg.sender);
         _mint(owner, 70000000 * (10 ** decimals()));
         blockReward = reward * (10 ** decimals());
@@ -36,10 +36,6 @@ contract CoffeeCode is ERC20Capped, ERC20Burnable {
 
     function setBlockReward(uint256 reward) public onlyOwner {
         blockReward = reward * (10 ** decimals());
-    }
-
-    function destroy() public onlyOwner {
-        selfdestruct(owner);
     }
 
     modifier onlyOwner {
