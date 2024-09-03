a0eaacc453d1694ca694f17427a38868c463b619
Znackt <Znackt@proton.me>
2024-09-03 23:30:50 +0530

    reconfigured hardhat after installing new packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts"
+  },
+  networks: {
+    hardhat: {},
+    localhost: {
+      url: "http://127.0.0.1:8545"
+    },
+    sepolia: {
+      url: process.env.INFURA_SEPOLIA_KEY,
+      accounts: [process.env.PRIVATE_KEY]
+    }
+    // Add other network configurations as needed
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
