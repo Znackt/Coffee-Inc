845c9dc71b30f5b9cd0e8fe5910dfd354712eeee
Znackt <Znackt@proton.me>
2024-09-03 16:41:49 +0530

    removed _beforeTokenTransfer fn and replaced it with _update fn

--- contracts/Coffee_Inc.sol
+++ contracts/Coffee_Inc.sol
@@ -14,15 +14,24 @@ contract CoffeeCode is ERC20Capped, ERC20Burnable {
         blockReward = reward * (10 ** decimals());
     }
 
-    function _mintMinerReward () internal {
-        _mint(block.coinbase, blockReward);
+    function _update(address from, address to, uint256 value) internal virtual override (ERC20Capped, ERC20) {
+        super._update(from, to, value);
+
+        if (from == address(0)) {
+            uint256 maxSupply = cap();
+            uint256 supply = totalSupply();
+            if (supply > maxSupply) {
+                revert ERC20ExceededCap(supply, maxSupply);
+            }
+        }
+
+        if (from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
+            _mintMinerReward();
+        }
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 value) internal virtual override {
-        if (from != address(0) && to != block.coinbase && block.coinbase != address(0)) {
-            _mintMinerReward;
-        }
-        super._beforeTokenTransfer(from, to, value);
+    function _mintMinerReward () internal {
+        _mint(block.coinbase, blockReward);
     }
 
     function setBlockReward(uint256 reward) public onlyOwner {
